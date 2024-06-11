54f579c7b8095c89cb3be5c216b0b25c331c6996
Chris Pennington <chris@codinginpublic.dev>
2024-06-10 21:06:46 -0600

    finished code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  experimental: {
+    actions: true,
+  },
+  trailingSlash: "never",
+  output: "server",
+  adapter: vercel(),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
+/// <reference path="../.astro/actions.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace App {
+  interface Locals {
+    userEmail: string | null;
+  }
+}
